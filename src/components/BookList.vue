c7711132630c16080f1026c2e77bfb2a66a48e4f
newmohib <newmohib@gmail.com>
2019-10-21 03:34:12 +0600

    initial commit with chaild component

---
+++ src/components/BookList.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+    <h1>{{title}}</h1>
+    <ul>
+         <book-item v-for="book in books" :book='book'></book-item>
+    </ul>
+    </div>
+</template>
+
+<script>
+import BookItem from './BookItem'
+export default {
+    name:'BookList',
+    components:{
+        BookItem
+    },
+    data(){
+        return{
+            title:'All Books',
+            books:[
+                    {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
+                    {title: 'American Gods', author: 'Neil Gaiman'},
+                    {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
+                ]
+        }
+    }
+}
+</script>
+
+<style>
+ h1,h2{
+     font-weight: normal
+ }
+ ul{
+     list-style-type: none;
+     padding: 0 ;
+ }
+
+
+</style>
